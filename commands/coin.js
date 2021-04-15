734df4eb7e263054c6cdc279cedf9638d94024ff
IsoVoyd <dubnersamuel@gmail.com>
2021-04-14 21:23:47 -0700

    if a user is in the server they will not be considered on the leaderboard

--- commands/coin.js
+++ commands/coin.js
@@ -80,7 +80,9 @@ const leaderboard = async (message) => {
 
   let userList = await User.find()
     .sort([["score", -1]])
-    .limit(5);
+    .limit(10);
+  userList = userList.filter(user => message.guild.member(user.userId) != undefined)
+  userList = userList.slice(0, 5)
   let members = await message.guild.members.fetch();
 
   let embed = new MessageEmbed()
