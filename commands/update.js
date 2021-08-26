528aa196b9d5a00be973361a7c1adfd406899162
IsoVoyd <dubnersamuel@gmail.com>
2021-08-25 20:06:57 -0700

    define client in update.js

--- commands/update.js
+++ commands/update.js
@@ -11,7 +11,7 @@ module.exports = {
     )
       return;
 
-    client.destroy();
+    interaction.client.destroy();
     console.log(
       `${interaction.member.displayName} has destroyed the client, updating local repository...`
     );
