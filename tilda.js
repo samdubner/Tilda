c14a8b651120166a81ac9feb5e59250e46a49dee
IsoVoyd <dubnersamuel@gmail.com>
2021-08-29 14:08:22 -0700

    add guild member intent

--- tilda.js
+++ tilda.js
@@ -1,7 +1,7 @@
 const { Client, Collection, Intents, MessageEmbed } = require("discord.js");
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
 });
 client.commands = new Collection();
 
