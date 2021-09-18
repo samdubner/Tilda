5c4e90a6fda975b43c0972681ed36fadc1dc6236
IsoVoyd <dubnersamuel@gmail.com>
2021-09-17 23:49:03 -0700

    add sellRarity and sellFish.js

---
+++ mainCommands/sellRarity.js
@@ -0,0 +1,54 @@
+const catchHelper = require("../helpers/catchHelper")
+const coin = require("../helpers/coinHelper")
+
+module.exports = {
+  name: "sellRarity",
+  description: "sell fish by their rarity",
+  options: [
+    {
+      type: "STRING",
+      name: "pond",
+      description: "the pond you'd like to fish from",
+      required: false,
+      choices: [
+        {
+          name: "Plain Pond",
+          value: "plain",
+        },
+        {
+          name: "Underground Pond",
+          value: "underground",
+        },
+        {
+          name: "Underworld Pond",
+          value: "underworld",
+        },
+        {
+          name: "Sky Pond",
+          value: "sky",
+        },
+        {
+          name: "Ancient Pond",
+          value: "ancient",
+        },
+        {
+          name: "Void Pond",
+          value: "void",
+        },
+      ],
+    },
+  ],
+  async execute(interaction) {
+    let user = await coin.checkInteraction(interaction);
+
+    let pond;
+
+    if (interaction.options.get("pond")) {
+      pond = interaction.options.get("pond").value
+    } else {
+      pond = "plain"
+    }
+  
+    catchHelper.catchFish(interaction, user, pond);
+  },
+};
