19112635e6fd8cb256c52ee496fb7b2ce3235e29
samdubner <dubnersamuel@gmail.com>
2022-02-04 20:40:20 -0800

    update leaderboard to show members from any server

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -6,12 +6,11 @@ module.exports = {
   name: "leaderboard",
   description: "Shows the coins of the top 5 users registered with Tilda!",
   async execute(interaction) {
-    let userList = await User.find()
+    let userScores = await User.find()
       .sort([["score", -1]])
       .limit(10);
 
-    userList = await removeNotInGuild(userList, interaction);
-    userList = userList.slice(0, 5);
+      userScores = userScores.slice(0, 5);
 
     let embed = new MessageEmbed()
       .setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`)
@@ -19,19 +18,19 @@ module.exports = {
       .setThumbnail("https://i.imgur.com/hPCYkuG.gif");
 
     let i = 1;
-    for (let user of userList) {
-      if (user.score == userList[0].score) {
-        let guildMember = await interaction.guild.members.fetch(user.userId);
+    for (let document of userScores) {
+      if (document.score == userScores[0].score) {
+        let user = await interaction.client.users.fetch(document.userId);
         embed.addField(
-          `${1}) ${guildMember.user.username}`,
-          ` 🎉 ${user.score} 🎉`,
+          `${1}) ${user.username}`,
+          ` 🎉 ${document.score} 🎉`,
           false
         );
       } else {
-        let guildMember = await interaction.guild.members.fetch(user.userId);
+        let user = await interaction.client.users.fetch(document.userId);
         embed.addField(
-          `${i}) ${guildMember.user.username}`,
-          `${user.score}`,
+          `${i}) ${user.username}`,
+          `${document.score}`,
           false
         );
       }
@@ -40,16 +39,4 @@ module.exports = {
 
     interaction.reply({ embeds: [embed] }).catch(console.error);
   },
-};
-
-removeNotInGuild = async (userList, interaction) => {
-  let newList = [];
-
-  for (let user of userList) {
-    guildMember = await interaction.guild.members.fetch(user.userId);
-
-    if (guildMember) newList.push(user);
-  }
-
-  return newList;
 };
