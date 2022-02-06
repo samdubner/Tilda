7487509fa3e739b337b15adb532a9822f047524b
samdubner <dubnersamuel@gmail.com>
2022-02-06 01:17:15 -0800

    format config.js

--- commands/config.js
+++ commands/config.js
@@ -1,40 +1,44 @@
 const guildHelper = require("../helpers/guildHelper");
 
 module.exports = {
-    name: "config",
-    description: "set up basic config for tilda to get started!",
-    options: [
+  name: "config",
+  description: "set up basic config for tilda to get started!",
+  options: [
+    {
+      type: "SUB_COMMAND",
+      name: "channel",
+      description:
+        "configure the channel you would like to restrict Tilda's commands to",
+      options: [
         {
-            type: "SUB_COMMAND",
-            name: "channel",
-            description: "configure the channel you would like to restrict Tilda's commands to",
-            options: [
-                {
-                    type: "CHANNEL",
-                    name: "channel",
-                    description: "the channel you would like to use",
-                    required: true
-                }
-            ]
-        }
-    ],
-    async execute(interaction) {
-        let subcommand = interaction.options.getSubcommand();
+          type: "CHANNEL",
+          name: "channel",
+          description: "the channel you would like to use",
+          required: true,
+        },
+      ],
+    },
+  ],
+  async execute(interaction) {
+    let subcommand = interaction.options.getSubcommand();
 
-        switch(subcommand) {
-            case "channel":
-                changeCommandChannel(interaction);
-                break;
-            default:
-                console.log("subcommand not found in config.js")
-                return;
-        }
+    switch (subcommand) {
+      case "channel":
+        changeCommandChannel(interaction);
+        break;
+      default:
+        console.log("subcommand not found in config.js");
+        return;
     }
-}
+  },
+};
 
 let changeCommandChannel = async (interaction) => {
-    let channel = interaction.options.getChannel("channel");
-    await guildHelper.setGuildChannel(channel)
+  let channel = interaction.options.getChannel("channel");
+  await guildHelper.setGuildChannel(channel);
 
-    interaction.reply(`This guild's bot channel was updated to #${channel.name}`)
-}
+  interaction.reply({
+    content: `This guild's bot channel was updated to #${channel.name}`,
+    ephemeral: true,
+  });
+};
