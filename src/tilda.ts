8c256e9ce06425c4f6ad7521d641f818a2700b6d
SamDubner <dubnersamuel@gmail.com>
2022-05-12 20:21:41 -0700

    pass client to notifyUsers() and send dm at 9pm pst

--- src/tilda.ts
+++ src/tilda.ts
@@ -38,8 +38,8 @@ import clientHelper from "./helpers/clientHelper";
 import coinHelper from "./helpers/coinHelper";
 import guildHelper from "./helpers/guildHelper";
 
-schedule.scheduleJob("0 8 * * *", async () => {
-  coinHelper.notifyUsers();
+schedule.scheduleJob("0 9 * * *", async () => {
+  coinHelper.notifyUsers(client);
 });
 
 schedule.scheduleJob("0 0 * * *", async () => {
